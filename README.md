4f5642eaf19582d6bce1b0e9627f23dd0a6ee93f
Cscharwitzl <clemens@scharwitzl.com>
2017-11-04 14:30:47 +0100

    change Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mediatrix-prototyp
+
+Libaries:
+*Ola
+*PHP-CPP
+
+TODO's:
+*install php-dev
+*install php-CPP
--- cpp/DMX.cpp
+++ cpp/DMX.cpp
@@ -1,8 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ola/DmxBuffer.h>
+#include <ola/Logging.h>
+#include <ola/client/StreamingClient.h>
+#include <iostream>
 
 #include <map>
 
 class DMX {
     int sendChannel(channels map<int,int>){
-
+        
     }
 };
