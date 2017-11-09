59979f54e244bb920e6ab1b17e0ed4c6ffea1349
Cscharwitzl <clemens@scharwitzl.com>
2017-11-09 21:33:34 +0100

    cpp

--- cpp/DMX.cpp
+++ cpp/DMX.cpp
@@ -77,16 +77,15 @@ class DMX : public Php::Base {
         }
 
         static void noBlackout(){
-            Php::Parameters c();
-
-            map<Php::Value, Php::Value> t;
-
-            c[1] = t;
+            map<Php::Value, Php::Value> c;
 
             for(int i = 0; i<=512; i++){
-                c[1].insert(pair <Php::Value, Php::Value> (i, 255));
+                c.insert(pair <Php::Value, Php::Value> (i, 255));
             }
-            sendChannel(c);
+
+            Php::Parameters erg(c);
+
+            sendChannel(erg);
         }
 
 };
