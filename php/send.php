23925db8c190c82c097db7794be7b85474794818
Cscharwitzl <clemens@scharwitzl.com>
2017-11-10 13:12:34 +0100

    php

--- php/send.php
+++ php/send.php
@@ -7,6 +7,12 @@ $value = $_GET["value"];
 
 $dmx = new DMX();
 
+$erg = array();
+
+foreach ($channel as $entry) {
+  $erg.push(array($entry["channel"] => $entry["value"]));
+}
+
 $dmx::sendChannel(array(
   0 => 255,
   $channel => $value
