8b099d26c25c4884f1a109532de9998305c9b7b1
Cscharwitzl <clemens@scharwitzl.com>
2017-11-10 13:24:57 +0100

    php

--- php/send.php
+++ php/send.php
@@ -11,10 +11,12 @@ $erg = array();
 
 print_r($channel);
 
+/*
 foreach ($channel as $entry) {
   print_r(array($entry["channel"] => $entry["value"]));
   $erg.push(array($entry["channel"] => $entry["value"]));
 }
+*/
 
 $dmx::sendChannel(array(
   0 => 255,
